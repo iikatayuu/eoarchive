ac0a4bb7ef4a03e9e179b70eb161a4e89e519158
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2023-08-07 22:49:34 +0800

    perf: add database structure

---
+++ setup/database.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Aug 07, 2023 at 02:45 PM
+-- Server version: 8.0.27
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `eoarchive`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_eo`
+--
+
+DROP TABLE IF EXISTS `tbl_eo`;
+CREATE TABLE IF NOT EXISTS `tbl_eo` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'EO ID',
+  `number` int NOT NULL COMMENT 'EO Number',
+  `series` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'EO Series',
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'EO Title/Description',
+  `filename` varchar(255) NOT NULL COMMENT 'EO PDF Original Filename',
+  `author` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'EO Author',
+  `author_position` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'EO Author Position',
+  `approved_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'EO Approved By',
+  `date_approved` datetime NOT NULL COMMENT 'EO Approved Date',
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'EO Date Created',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COMMENT='List of Executive Orders';
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
