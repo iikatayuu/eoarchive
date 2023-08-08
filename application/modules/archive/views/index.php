c0d78356e407ee5e462f60b21f8f758649abd403
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2023-08-08 13:36:54 +0800

    chore: use long name as placeholder

--- application/modules/archive/views/index.php
+++ application/modules/archive/views/index.php
@@ -8,7 +8,7 @@ page_header('EO Archive', $scripts);
 <div class="container pt-4">
   <form id="archive-search" class="mb-3">
     <div class="input-group">
-      <input type="search" id="archive-search-q" class="form-control" placeholder="EO Title/Description" aria-label="EO Title/Description" aria-describedby="btn-item-search" />
+      <input type="search" id="archive-search-q" class="form-control" placeholder="Executive Order Title/Description" aria-label="Executive Order Title/Description" aria-describedby="btn-item-search" />
       <button type="submit" class="btn btn-success" id="btn-eo-search">
         <i class="fas fa-search me-1"></i>
         <span>Search</span>
