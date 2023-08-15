b08ee378410bb604227325ed45f0f02261f8b96a
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2023-08-15 21:38:33 +0800

    perf: update database structure

--- setup/database.sql
+++ setup/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Aug 07, 2023 at 02:45 PM
+-- Generation Time: Aug 15, 2023 at 01:37 PM
 -- Server version: 8.0.27
 -- PHP Version: 7.4.26
 
@@ -24,6 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_admins`
+--
+
+DROP TABLE IF EXISTS `tbl_admins`;
+CREATE TABLE IF NOT EXISTS `tbl_admins` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'Account ID',
+  `username` varchar(255) NOT NULL COMMENT 'Account username',
+  `password` varchar(72) NOT NULL COMMENT 'Account hashed password',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+
+--
+-- Dumping data for table `tbl_admins`
+--
+
+INSERT INTO `tbl_admins` (`id`, `username`, `password`) VALUES
+(1, 'admin', '$2y$10$Hgn1zM0cucafwi0M8ayO.Ohf4LPgR.yH0G228YnfgMH8g9YDsGcr2');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_eo`
 --
 
@@ -37,10 +58,22 @@ CREATE TABLE IF NOT EXISTS `tbl_eo` (
   `author` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'EO Author',
   `author_position` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'EO Author Position',
   `approved_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'EO Approved By',
+  `added_by` int NOT NULL COMMENT 'Added by',
   `date_approved` date NOT NULL COMMENT 'EO Approved Date',
   `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'EO Date Created',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `ADMIN_FK_1` (`added_by`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COMMENT='List of Executive Orders';
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_eo`
+--
+ALTER TABLE `tbl_eo`
+  ADD CONSTRAINT `ADMIN_FK_1` FOREIGN KEY (`added_by`) REFERENCES `tbl_admins` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
